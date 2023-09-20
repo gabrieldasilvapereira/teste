83d2991aadd880f7ceed7332879e7bb2cb11a7d3
Gabriel da Silva Pereira <gabriel.pereira503@etec.sp.gov.br>
2023-09-20 20:28:56 -0300

    aula 1 repo linguagem C

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Boa noite\n");
+	printf("prova na proxima semana =(\n");
+	printf("Hello World\n");
+	return 0;
+}
